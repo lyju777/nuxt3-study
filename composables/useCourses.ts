399e81b6e9439b299906e4580da39e34ae8b9f98
lyju777 <lyju777@hanmail.net>
2025-07-29 16:37:30 +0900

    15. Course 데이터 가져오기

---
+++ composables/useCourses.ts
@@ -0,0 +1,21 @@
+import coursesData from "./coursesData";
+import type { CourseWithPath } from "~/types/course";
+
+interface CoursesReturn {
+  courses: CourseWithPath[];
+}
+
+export const useCourses = (): CoursesReturn => {
+  const courses = coursesData.map((item) => {
+    return {
+      ...item,
+      rating: item.rating.toFixed(1),
+      reviewsCount: item.reviewsCount.toLocaleString(),
+      studentCount: item.studentCount.toLocaleString(),
+      path: `/course/${item.courseSlug}`,
+    };
+  });
+  return {
+    courses,
+  };
+};
