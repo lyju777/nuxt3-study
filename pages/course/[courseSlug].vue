67f9bfd2ec95358357d497531b972a924939bd79
lyju777 <lyju777@hanmail.net>
2025-07-30 14:29:03 +0900

    18. 라우팅 고급 - 중첩 라우팅

--- pages/course/[courseSlug].vue
+++ pages/course/[courseSlug].vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="q-pa-xl">
+  <div>
     <AppCard>
       <template #header>
         <div class="text-h5 text-weight-medium">{{ course?.title }}</div>
@@ -43,6 +43,27 @@
         </div>
       </div>
       <p class="q-mt-lg text-grey-8">{{ course?.content }}</p>
+      <q-separator class="q-mb-lg" />
+      <q-form class="q-gutter-y-md">
+        <q-btn
+          label="수강완료"
+          class="full-width"
+          color="green"
+          unelevated
+          :outline="completed ? false : true"
+          :icon="completed ? 'check' : undefined"
+          @click="completed = !completed"
+        />
+        <q-input
+          v-model="memo"
+          type="textarea"
+          outlined
+          dense
+          placeholder="메모를 작성해주세요."
+          rows="3"
+          autogrow
+        />
+      </q-form>
       <template #footer>
         <ClientOnly>
           <q-btn
@@ -74,8 +95,12 @@ definePageMeta({
   key: (route) => route.fullPath,
   title: "My home page",
   pageType: "course",
+  keepalive: true,
+  alias: ["/lecture/courseSlug"],
 });
-console.log(`route.meta`, route.meta);
+
+const memo = ref("");
+const completed = ref(false);
 </script>
 
 <style scoped></style>
