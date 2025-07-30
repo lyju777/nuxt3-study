248fe1cc821696e36e5521c10a25845915262551
lyju777 <lyju777@hanmail.net>
2025-07-30 17:16:07 +0900

    25. Nuxt plugins(다국어) & utils

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,12 +4,59 @@
       <q-toolbar>
         <q-toolbar-title> Vue & Nuxt Mastery Class </q-toolbar-title>
         <NuxtLink v-slot="{ navigate }" custom to="/">
-          <q-btn stretch flat label="Home" no-caps @click="() => navigate()" />
+          <q-btn
+            stretch
+            flat
+            :label="$t('home')"
+            no-caps
+            @click="() => navigate()"
+          />
         </NuxtLink>
         <q-separator dark vertical />
         <NuxtLink v-slot="{ navigate }" custom to="/about">
-          <q-btn stretch flat label="About" no-caps @click="() => navigate()" />
+          <q-btn
+            stretch
+            flat
+            :label="$t('about')"
+            no-caps
+            @click="() => navigate()"
+          />
         </NuxtLink>
+        <NuxtLink v-slot="{ navigate }" custom to="/youtube">
+          <q-btn
+            stretch
+            flat
+            :label="$t('youtube')"
+            no-caps
+            @click="() => navigate()"
+          />
+        </NuxtLink>
+        <NuxtLink v-slot="{ navigate }" custom to="/admin">
+          <q-btn
+            stretch
+            flat
+            :label="$t('admin')"
+            no-caps
+            @click="() => navigate()"
+          />
+        </NuxtLink>
+        <q-separator dark vertical />
+        <q-btn-dropdown stretch flat no-caps :label="selectedLanguageName">
+          <q-list padding dense>
+            <q-item
+              v-for="{ code, name } in languages"
+              :key="code"
+              v-close-popup
+              clickable
+              :active="code === $i18n.locale"
+              @click="$i18n.locale = code"
+            >
+              <q-item-section>
+                <q-item-label>{{ name }}</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
       </q-toolbar>
     </q-header>
     <q-page-container :style="pageContainerStyle">
@@ -22,4 +69,21 @@ const pageContainerStyle = computed(() => ({
   maxWidth: "1080px",
   margin: "0 auto",
 }));
+
+interface Language {
+  name: string;
+  code: "en" | "ko";
+}
+
+const languages = ref<Language[]>([
+  { name: "English", code: "en" },
+  { name: "한국어", code: "ko" },
+]);
+
+const { locale } = useI18n();
+
+const selectedLanguageName = computed(() => {
+  return languages.value.find((language) => language.code === locale.value)
+    ?.name;
+});
 </script>
---
+++ pages/youtube.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page padding>
+    <div class="q-my-xl text-center">
+      <div class="text-h4">youtube</div>
+      <p class="q-mt-sm text-grey-8">
+        Vue & Nuxt Mastery Class 웹은 Nuxt3로 만들어졌습니다.
+      </p>
+      <div class="column">
+        <RouterLink to="/">Router Link Home</RouterLink>
+        <RouterLink to="https://www.naver.com">Router Link Naver</RouterLink>
+        <br />
+        <NuxtLink to="/">NuxtLink Home</NuxtLink>
+        <NuxtLink to="https://www.naver.com">NuxtLink Naver</NuxtLink>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
